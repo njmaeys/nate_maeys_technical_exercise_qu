671fe42f5cae502a9f3801e1d0bd7cb877572840
njmaeys <nmaeys@hotmail.com>
2025-04-24 15:21:28 -0500

    Finalizing

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,8 @@ WORKDIR /code
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
-#CMD ["uvicorn","app.main:app","--host","0.0.0.0","--port","8000"]
-CMD ["uvicorn","app.main:app","--host","0.0.0.0","--port","8000", "--reload"]
+CMD ["uvicorn","app.main:app","--host","0.0.0.0","--port","8000"]
+
+# I'm leaving this in here because having the --reolad is nice for local dev
+# it isn't a good idea for production but keeping it to show I used it
+#CMD ["uvicorn","app.main:app","--host","0.0.0.0","--port","8000", "--reload"]
--- services/insights/ts_client.py
+++ services/insights/ts_client.py
@@ -4,6 +4,13 @@ from datetime import datetime
 # I had to make this index [2] to read my file... not sure why but it worked instead of [3]
 DATA=json.loads((pathlib.Path(__file__).parents[2]/'data/mock_timeseries.json').read_text())
 
+"""
+NOTE:
+I'm not sure if I was supposed to mess with this client but I wouldn't want to make my API
+do the filtering on a massive data set when I could make the TimeSeries client do it.
+
+I assumed it was ok to do so since the start and end params were here but doing nothing...
+"""
 def query(duid: str, circuit: int, start: str, end: str):
     start_dt = datetime.fromisoformat(start.replace("Z", "+00:00"))
     end_dt = datetime.fromisoformat(end.replace("Z", "+00:00"))
